9bd5805deb3078f83ce161f700d6cd897d6aeb7a
acw0 <acw@ml1.net>
2020-07-10 23:27:53 -0400

    typescript rewrite, does not compile

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    description: "Check if the bot is running!",
-    permitted: (ctx) => true,
-    execute: async function (ctx) {
-        await ctx.reply(`Hi! I've been running for ${Math.floor(process.uptime())} seconds.`);
-    }
-}
